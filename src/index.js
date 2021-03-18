a29df72cdd6f560ccd8c2c2777b57810df575b54
Justin Noel <justin.noel@calendee.com>
2021-03-18 07:44:16 -0500

    feat: add itty-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-addEventListener("fetch", event => {
-	event.respondWith(handleRequest(event.request));
-});
+import { Router } from "itty-router";
 
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-	return new Response("Hello worker!", {
-		headers: { "content-type": "text/plain" },
-	});
-}
+// create a router
+const router = Router(); // this is a Proxy, not a class
+
+// GET collection index
+router.get("/notes", () => new Response("Notes Index!"));
+
+// GET item
+router.get("/notes/:id", ({ params }) => new Response(`Notes #${params.id}`));
+
+// 404 for everything else
+router.all("*", () => new Response("Not Found.", { status: 404 }));
+
+// attach the router "handle" to the event handler
+addEventListener("fetch", event =>
+	event.respondWith(router.handle(event.request)),
+);
